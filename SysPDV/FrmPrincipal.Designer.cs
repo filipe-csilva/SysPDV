7d100a39e2bf6d49725a5090389d8a54e3617cc6
filipeleasing <filipepaulocs@gmail.com>
2023-02-17 23:10:47 -0300

    Adicionar arquivos de projeto.

---
+++ SysPDV/FrmPrincipal.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace SysPDV
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.MenuCadastro = new System.Windows.Forms.ToolStripMenuItem();
+            this.produtosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.movimentaçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatóriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.img01 = new System.Windows.Forms.PictureBox();
+            this.img02 = new System.Windows.Forms.PictureBox();
+            this.img03 = new System.Windows.Forms.PictureBox();
+            this.img04 = new System.Windows.Forms.PictureBox();
+            this.MenuPrincipal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img03)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img04)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MenuPrincipal
+            // 
+            this.MenuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuCadastro,
+            this.produtosToolStripMenuItem,
+            this.movimentaçõesToolStripMenuItem,
+            this.relatóriosToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.MenuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.MenuPrincipal.Name = "MenuPrincipal";
+            this.MenuPrincipal.Size = new System.Drawing.Size(800, 24);
+            this.MenuPrincipal.TabIndex = 0;
+            this.MenuPrincipal.Text = "menuStrip1";
+            // 
+            // MenuCadastro
+            // 
+            this.MenuCadastro.Name = "MenuCadastro";
+            this.MenuCadastro.Size = new System.Drawing.Size(66, 20);
+            this.MenuCadastro.Text = "Cadastro";
+            // 
+            // produtosToolStripMenuItem
+            // 
+            this.produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
+            this.produtosToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.produtosToolStripMenuItem.Text = "Produtos";
+            // 
+            // movimentaçõesToolStripMenuItem
+            // 
+            this.movimentaçõesToolStripMenuItem.Name = "movimentaçõesToolStripMenuItem";
+            this.movimentaçõesToolStripMenuItem.Size = new System.Drawing.Size(104, 20);
+            this.movimentaçõesToolStripMenuItem.Text = "Movimentações";
+            // 
+            // relatóriosToolStripMenuItem
+            // 
+            this.relatóriosToolStripMenuItem.Name = "relatóriosToolStripMenuItem";
+            this.relatóriosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.relatóriosToolStripMenuItem.Text = "Relatórios";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // img01
+            // 
+            this.img01.Location = new System.Drawing.Point(120, 50);
+            this.img01.Name = "img01";
+            this.img01.Size = new System.Drawing.Size(158, 134);
+            this.img01.TabIndex = 1;
+            this.img01.TabStop = false;
+            // 
+            // img02
+            // 
+            this.img02.Location = new System.Drawing.Point(284, 50);
+            this.img02.Name = "img02";
+            this.img02.Size = new System.Drawing.Size(158, 134);
+            this.img02.TabIndex = 2;
+            this.img02.TabStop = false;
+            // 
+            // img03
+            // 
+            this.img03.Location = new System.Drawing.Point(120, 190);
+            this.img03.Name = "img03";
+            this.img03.Size = new System.Drawing.Size(158, 134);
+            this.img03.TabIndex = 3;
+            this.img03.TabStop = false;
+            // 
+            // img04
+            // 
+            this.img04.Location = new System.Drawing.Point(284, 190);
+            this.img04.Name = "img04";
+            this.img04.Size = new System.Drawing.Size(158, 134);
+            this.img04.TabIndex = 4;
+            this.img04.TabStop = false;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.img04);
+            this.Controls.Add(this.img03);
+            this.Controls.Add(this.img02);
+            this.Controls.Add(this.img01);
+            this.Controls.Add(this.MenuPrincipal);
+            this.MainMenuStrip = this.MenuPrincipal;
+            this.Name = "frmPrincipal";
+            this.Text = "Principal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MenuPrincipal.ResumeLayout(false);
+            this.MenuPrincipal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img03)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img04)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem MenuCadastro;
+        private System.Windows.Forms.ToolStripMenuItem produtosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem movimentaçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatóriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.PictureBox img01;
+        private System.Windows.Forms.PictureBox img02;
+        private System.Windows.Forms.PictureBox img03;
+        private System.Windows.Forms.PictureBox img04;
+    }
+}
+
